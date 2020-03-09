085beafb50a16ed27cae8c2bfe9e0d8cffa00036
Nikhil Kataria <44164498+katarianikhilk@users.noreply.github.com>
2020-03-09 22:25:51 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -133,6 +133,6 @@ LOGIN_URL = 'login'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'katarianikhilk@gmail.com'
-EMAIL_HOST_PASSWORD = 'abhitrykaro'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
