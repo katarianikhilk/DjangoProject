29cdd9c78af3d07d45db1ee597005933255debab
nikhil <nikhil.k18@iiits.in>
2020-03-12 21:48:16 +0530

    Home pag

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'blog/home.html')
+
+
+def about(request):
+    return render(request, 'blog/aboutus.html')
+
+
+def menu(request):
+    return render(request, 'blog/menu.html')
+
+
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+
--- project/urls.py
+++ project/urls.py
@@ -5,5 +5,10 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('menu/', views.menu, name='menu'),
+    path('contact/', views.contact, name='contact'),
+
+
 ]
